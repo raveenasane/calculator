849f56475173aa1a8523697fc57709511994796c
saneraveena <raveenasane@gmail.com>
2023-08-16 15:11:55 +0530

    calculator

---
+++ src/App.js
@@ -0,0 +1,42 @@
+
+import React,{useState} from "react";
+import './App.css';
+const App=()=>{
+  const[input,setInput]=useState("");
+  const[result,setResult]=useState(0);
+  const Handler=(e)=>{
+    setInput(e.target.value);
+  }
+  function del(){
+    setInput(input.slice(0,-1));
+  }
+  return(
+    <div className='main'>
+      <center>
+         <input type="text" value={input} name="input" onChange={Handler}/>
+         <br/><br/>
+         <button onClick={()=>setResult(eval(input))}>Result</button><br/>
+         <h2 className="result">{result}</h2><br/>
+         <button onClick={del}>Delete</button>&nbsp;
+         <button onClick={()=>setInput(" ")}>Clear</button><br/>
+         <button onClick={()=>setInput(input+"7")}>7</button>
+         <button onClick={()=>setInput(input+"8")}>8</button>
+         <button onClick={()=>setInput(input+"9")}>9</button>
+         <button onClick={()=>setInput(input+"*")}>x</button><br/>
+         <button onClick={()=>setInput(input+"4")}>4</button>
+         <button onClick={()=>setInput(input+"5")}>5</button>
+         <button onClick={()=>setInput(input+"6")}>6</button>
+         <button onClick={()=>setInput(input+"-")}>-</button><br/>
+         <button onClick={()=>setInput(input+"1")}>1</button>
+         <button onClick={()=>setInput(input+"2")}>2</button>
+         <button onClick={()=>setInput(input+"3")}>3</button>
+         <button onClick={()=>setInput(input+"+")}>+</button><br/>
+         <button onClick={()=>setInput(input+"00")}>00</button>
+         <button onClick={()=>setInput(input+"0")}>0</button>
+         <button onClick={()=>setInput(input+".")}>.</button>
+         <button onClick={()=>setInput(input+"=")}>=</button><br/>
+      </center>
+    </div> 
+  )
+}
+export default App;
